3b3d9f2aed95da16bbc3c0d65bc84f2bbc2f39c4
Sydnie Fenske <Sydnie@Sydnies-MacBook-Pro.local>
2017-04-03 11:58:25 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Sydnie Fenske on 4/3/17.
+//  Copyright © 2017 Sydnie Fenske. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
